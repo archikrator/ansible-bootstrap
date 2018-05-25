157f1fbf2dec9d1f880d328bee9b1cdfacf84c20
Szymon Kempny <szymon@kempny.pl>
2018-05-26 00:52:13 +0200

    init

---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Script to initialize Ansible environment
+#
+# Usage: source init.sh
+#
+
+# Check if the path is corrent
+[[ -f "./.ansible_source_marker" ]] || {
+  echo "You run this script from bath path,"
+  echo "cd to you ansible repository and try again."
+  return
+}
+
+pipenv install
+
+# Copy git hooks
+cp .githooks/* .git/hooks/
+
+BASE_ANSIBLE_REPO_PATH=$(pwd)
+PATH=$BASE_ANSIBLE_REPO_PATH/.bin:$PATH
+
+export PATH
+# Can be used in lookup module when playbooks are in different folders
+# Example usage:
+# base_ansible_repo_path: "{{ lookup('env', 'BASE_ANSIBLE_REPO_PATH') }}"
+# ssh_public_key: "{{ lookup('file', base_ansible_repo_path+'/ssh_keys/user.pub') }}"
+export BASE_ANSIBLE_REPO_PATH
+
+pipenv shell
